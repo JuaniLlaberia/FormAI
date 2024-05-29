3f314ab5a1208d005bd8a0176aebdd485459355b
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-05-29 20:18:55 -0300

    Update dashboard

--- src/app/dashboard/form/[formId]/edit/page.tsx
+++ src/app/dashboard/form/[formId]/edit/page.tsx
@@ -1,15 +1,19 @@
-import { redirect } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
+
+import FormEditor from './(components)/FormEditor';
 import { getFormById } from '@/actions/forms';
 
 const FormEditPage = async ({ params }: { params: { formId: string } }) => {
   const form = await getFormById(params.formId);
 
-  if (form?.needsConfigure)
+  if (!form) return notFound();
+
+  if (form.needsConfigure)
     return redirect(`/dashboard/form/${form.id}/${form.configureType}`);
+  if (form.isPublished)
+    return redirect(`/dashboard/form/${form.id}/information`);
 
-  console.log(form);
-
-  return <div></div>;
+  return <FormEditor form={form} />;
 };
 
 export default FormEditPage;
