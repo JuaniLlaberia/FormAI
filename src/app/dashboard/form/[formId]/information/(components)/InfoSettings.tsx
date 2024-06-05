ef4efbf7bfab0666f776e79f2079c1f7ee2b4ea1
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-06-05 10:15:21 -0300

    Add download data functionalities

--- src/app/dashboard/form/[formId]/information/(components)/InfoSettings.tsx
+++ src/app/dashboard/form/[formId]/information/(components)/InfoSettings.tsx
@@ -1,5 +1,10 @@
-import { Copy, Download, Settings, Trash } from 'lucide-react';
+'use client';
 
+import { parse } from 'json2csv';
+import { saveAs } from 'file-saver';
+import { Download, Settings, Trash } from 'lucide-react';
+
+import DeleteFormModal from '@/app/dashboard/(components)/DeleteFormModal';
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
@@ -8,34 +13,78 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
+import { Column, Row } from '../page';
 
-const InfoSettings = () => {
+const InfoSettings = ({
+  formId,
+  formName,
+  columns,
+  data,
+}: {
+  formId: string;
+  formName: string;
+  columns: Column[];
+  data: Row[];
+}) => {
+  const exportToCSV = (columns: Column[], data: Row[]) => {
+    const labels = columns.map(col => col.label);
+    const csvData = data.map(row => Object.values(row));
+
+    const csvArray = [labels, ...csvData];
+
+    const csv = parse(csvArray);
+    const blob = new Blob([csv], { type: 'text/csv;charset=utf-8' });
+
+    saveAs(blob, 'data.csv');
+  };
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <div>
-          <Button size='icon' variant='outline' className='md:hidden'>
-            <Settings className='size-5' />
-          </Button>
-          <Button size='sm' variant='outline' className='hidden md:flex'>
-            <Settings className='size-4 mr-1.5' />
-            Settings
-          </Button>
-        </div>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuItem>
-          <Copy className='size-4 mr-1.5' /> Copy link
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <Download className='size-4 mr-1.5' /> Download data
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem className='text-red-400 hover:text-red-500 focus:text-red-500'>
-          <Trash className='size-4 mr-1.5' /> Remove
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <Dialog>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div>
+              <Button
+                size='icon'
+                variant='outline'
+                className='md:hidden'
+              >
+                <Settings className='size-5' />
+              </Button>
+              <Button
+                size='sm'
+                variant='outline'
+                className='hidden md:flex'
+              >
+                <Settings className='size-4 mr-1.5' />
+                Settings
+              </Button>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem onClick={() => exportToCSV(columns, data)}>
+              <Download className='size-4 mr-1.5' /> Download data
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className='text-red-400 hover:text-red-500 focus:text-red-500 w-full'
+              asChild
+            >
+              <DialogTrigger>
+                <Trash className='size-4 mr-1.5' /> Remove
+              </DialogTrigger>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <DialogContent>
+          <DeleteFormModal
+            formId={formId}
+            formName={formName}
+          />
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
